03ce0c42717b3c02c4a9fdd63d648d835493c350
putriinadhifah <putrinadhifah06@gmail.com>
2025-12-18 09:57:44 +0700

    view kategori

--- src/main/App.java
+++ src/main/App.java
@@ -3,12 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package App;
+package main;
 
 /**
  *
  * @author NATA
  */
-public class java {
+public class App {
     
 }
